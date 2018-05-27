7dffa0090cc6b779dd3f6ea5fb551503c035e00f
Junha Roh <rohjunha@gmail.com>
2018-05-27 13:30:10 -0700

    modified to use python3

--- master/master.py
+++ master/master.py
@@ -1,102 +1,108 @@
 import socket
 import sys
-from datetime import datetime
 import time
 
 server_socket = None
-ADDRESS = "10.0.0.19"
-#ADDRESS = "192.168.137.2"
-#ADDRESS = "127.0.0.1"
+# ADDRESS = '10.0.0.19'
+# ADDRESS = '192.168.137.2'
+ADDRESS = '127.0.0.1'
 PORT = 2468
 NUM_OF_TIMES = 1000
 
 OFFSETS = []
 DELAYS = []
 
+
 def main():
-  try:
-    global server_socket
-    print "Creating socket..."
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  except socket.error as e:
-    print "Error creating socket: " + str(e) + ". Exitting..."
-    server_socket.close();
-    sys.exit(-1)
+    try:
+        global server_socket
+        print('Creating socket...')
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    except socket.error as e:
+        print('Error creating socket: {}'.format(e))
+        server_socket.close()
+        sys.exit(-1)
 
-  try:
-    print "Connecting to socket... " + str(ADDRESS) + ":" + str(PORT)
-    server_socket.connect((ADDRESS, PORT))
-  except socket.error as e:
-    print "Error connecting to socket: " + e + ". Exitting..."
-    server_socket.close()
-    sys.exit(-1)
+    try:
+        print('Connecting to socket... {}:{}'.format(ADDRESS, PORT))
+        server_socket.connect((ADDRESS, PORT))
+    except socket.error as e:
+        print('Error connecting to socket: {}'.format(e))
+        server_socket.close()
+        sys.exit(-1)
 
-  sync_clock()
-  
+    sync_clock()
+
+
 def sync_clock():
-  print "\nSyncing time with " + ADDRESS + ":" + str(PORT) + "..."
-  send("sync")
-  t, resp = recv()
-  send(str(NUM_OF_TIMES))
+    print('Syncing time with {}:{}'.format(ADDRESS, PORT))
+    send('sync')
+    t, resp = recv()
+    send(str(NUM_OF_TIMES))
 
-  t, resp = recv()
+    t, resp = recv()
+    resp = resp.decode()
 
-  if(resp == "ready"):
-    time.sleep(1) #to allow for server to get ready
-    for i in range(NUM_OF_TIMES):
-      ms_diff = sync_packet()
-      sm_diff = delay_packet()
+    if resp == 'ready':
+        time.sleep(1)  # to allow for server to get ready
+        for i in range(NUM_OF_TIMES):
+            ms_diff = sync_packet()
+            sm_diff = delay_packet()
 
-      offset = (ms_diff - sm_diff)/2;
-      delay = (ms_diff + sm_diff)/2;
+            offset = (ms_diff - sm_diff) / 2
+            delay = (ms_diff + sm_diff) / 2
 
-      OFFSETS.append(offset)
-      DELAYS.append(delay)
+            OFFSETS.append(offset)
+            DELAYS.append(delay)
 
-      send("next")
+            send('next')
 
-    print "\n\nAVG OFFSET: %sns" % str(sum(OFFSETS) * 1000000000L / len(OFFSETS)) + "\nAVG DELAY: %sns"% str(sum(DELAYS) * 1000000000L / len(DELAYS))
-    print "\n\nMIN OFFSET: %sns" % str(min(OFFSETS) * 1000000000L) + "\nMIN DELAY: %sns"% str(min(DELAYS) * 1000000000L)
-    print "\n\nMAX OFFSET: %sns" % str(max(OFFSETS) * 1000000000L) + "\nMAX DELAY: %sns"% str(max(DELAYS) * 1000000000L)
-    print "\nDone!"
-  else:
-    print "Error syncing times, received: " + resp
+        print('avg offset: {}'.format(sum(OFFSETS) * 1e9 / len(OFFSETS)))
+        print('avg delay : {}'.format(sum(DELAYS) * 1e9 / len(DELAYS)))
+        print('min offset: {}'.format(min(OFFSETS) * 1e9 / len(OFFSETS)))
+        print('max offset: {}'.format(max(OFFSETS) * 1e9 / len(OFFSETS)))
+    else:
+        print('Error syncing times, received: {}'.format(resp))
+
 
 def sync_packet():
-  t1 = send("sync_packet")
-  t, t2 = recv()
-  return float(t2) - float(t1)
+    t1 = send('sync_packet')
+    t, t2 = recv()
+    return float(t2) - float(t1)
+
 
 def delay_packet():
-  send("delay_packet")
-  t4, t3 = recv()
-  return float(t4) - float(t3)
+    send('delay_packet')
+    t4, t3 = recv()
+    return float(t4) - float(t3)
 
 
 def recv():
-  try:
-    msg = server_socket.recv(4096)
-    t = get_time()
-    return (t, msg)
-  except socket.error as e:
-    print "Error while receiving request: " + str(e)
-    server_socket.close()
-    sys.exit(-1)
+    try:
+        msg = server_socket.recv(4096)
+        t = get_time()
+        return t, msg
+    except socket.error as e:
+        print('Error while receiving request: {}'.format(e))
+        server_socket.close()
+        sys.exit(-1)
+
 
 def send(data):
-  try:
-    server_socket.sendall(str(data))
-    t = get_time()
-    return t
-    #print "Sent:" + str(data)
-  except socket.error as e:
-    print "Error while sending request: " + str(e)
-    print "Tried to send: " + data
-    server_socket.close()
-    sys.exit(-1)
+    try:
+        server_socket.sendall(str(data).encode('utf-8'))
+        t = get_time()
+        return t
+    except socket.error as e:
+        print('Error while sending request: {}'.format(e))
+        print('Tried to send: {}'.format(data))
+        server_socket.close()
+        sys.exit(-1)
+
 
 def get_time():
-  return time.time()
+    return time.time()
+
 
 if __name__ == '__main__':
     main()
--- slave/slave.py
+++ slave/slave.py
@@ -1,87 +1,92 @@
 import socket
 import sys
-from datetime import datetime
 import time
 
 server_socket = None
-ADDRESS = ""
+ADDRESS = ''
 PORT = 2468
 
+
 def main():
-  global server_socket
-  try:
-    print "Creating socket..."
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  except socket.error as e:
-    print "Error creating socket: " + str(e) + ". Exitting..."
-    server_socket.close();
-    sys.exit(-1)
+    global server_socket
+    try:
+        print('Creating socket...')
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    except socket.error as e:
+        print('Error creating socket: ' + str(e) + '. Exitting...')
+        server_socket.close()
+        sys.exit(-1)
 
-  try:
-    print "Binding to socket... " + str(ADDRESS) + ":" + str(PORT)
-    server_socket.bind((ADDRESS, PORT))
-  except socket.error as e:
-    print "Error binding to socket: " + str(e) + ". Exitting..."
-    server_socket.close()
-    sys.exit(-1)
+    try:
+        print('Binding to socket... ' + str(ADDRESS) + ':' + str(PORT))
+        server_socket.bind((ADDRESS, PORT))
+    except socket.error as e:
+        print('Error binding to socket: ' + str(e) + '. Exitting...')
+        server_socket.close()
+        sys.exit(-1)
 
-  try:
-    while(1):
-      print "\nReady to receive requests on port " + str(PORT) + "..."
-      data, addr = server_socket.recvfrom(4096)
-      print "Request from " + addr[0]
-      if("sync" == data):
-        server_socket.sendto("ready", addr)
-        num_of_times, addr = server_socket.recvfrom(4096)
-        server_socket.sendto("ready", addr)
-        for i in range(int(num_of_times)):
-          sync_clock();
-        print "Done!"
-      else:
-        server_socket.sendto("Hello World!", addr)
-  except socket.error as e:
-    print "Error while handling requests: " + str(e)
-    server_socket.close()
-    sys.exit(-1)
+    try:
+        while True:
+            print('\nReady to receive requests on port ' + str(PORT) + '...')
+            data, addr = server_socket.recvfrom(4096)
+            data = data.decode()
+            print('Request from ' + addr[0])
+            if 'sync' == data:
+                server_socket.sendto('ready'.encode('utf-8'), addr)
+                num_of_times, addr = server_socket.recvfrom(4096)
+                server_socket.sendto('ready'.encode('utf-8'), addr)
+                for i in range(int(num_of_times)):
+                    sync_clock()
+                print('Done!')
+            else:
+                server_socket.sendto('Hello World!'.encode('utf-8'), addr)
+    except socket.error as e:
+        print('Error while handling requests: ' + str(e))
+        server_socket.close()
+        sys.exit(-1)
+
 
 def sync_clock():
-  addr = sync_packet();
-  delay_packet(addr)
-  recv()
+    addr = sync_packet()
+    delay_packet(addr)
+    recv()
+
 
 def sync_packet():
-  t2, (t1, addr) = recv()
-  send(t2, addr)
-  return addr
+    t2, (t1, addr) = recv()
+    send(t2, addr)
+    return addr
+
 
 def delay_packet(addr):
-  recv()
-  send(get_time(), addr)
+    recv()
+    send(get_time(), addr)
 
 
 def recv():
-  try:
-    request = server_socket.recvfrom(4096)
-    t = get_time()
-    #print "Request from " + str(request[1][0])
-    return (t, request)
-  except socket.error as e:
-    print "Error while receiving request: " + str(e)
-    server_socket.close()
-    sys.exit(-1)
+    try:
+        request = server_socket.recvfrom(4096)
+        t = get_time()
+        return t, request
+    except socket.error as e:
+        print('Error while receiving request: ' + str(e))
+        server_socket.close()
+        sys.exit(-1)
+
 
 def send(data, addr):
-  try:
-    server_socket.sendto(str(data), addr)
-    #print "Sent to " + addr[0]
-  except socket.error as e:
-    print "Error while sending request: " + str(e)
-    print "Tried to send: " + data
-    server_socket.close()
-    sys.exit(-1)
+    try:
+        server_socket.sendto(str(data).encode('utf-8'), addr)
+    except socket.error as e:
+        print('Error while sending request: ' + str(e))
+        print('Tried to send: ' + data)
+        server_socket.close()
+        sys.exit(-1)
+
 
 def get_time():
-  return time.time()
+    return time.time()
+
 
 if __name__ == '__main__':
     main()
